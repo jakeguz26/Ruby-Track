9c353e47973519deef60e38a9194d24205b511ea
Jake Guzman <jacobguz26@gmail.com>
2022-09-08 14:35:12 -0700

    Worked on calculator exercise from Lesson 2 part 11

--- RB101/Lesson_2/calculator.rb
+++ RB101/Lesson_2/calculator.rb
@@ -3,28 +3,103 @@
 # perform the operatorion on the two numbers
 # output the result
 
-puts "Welcome to the calculator!"
-
-puts "Please input two numbers"
-
-print "Number #1: "
-user_input_number_1 = gets.chomp.to_i
-
-print "Number #2: "
-user_input_number_2 = gets.chomp.to_i
-
-puts "What kind of operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide "
-user_input_operation = gets.chomp
-
-case user_input_operation
-when "add"
-	result = user_input_number_1 + user_input_number_2
-when "subtract"
-	result = user_input_number_1 - user_input_number_2
-when "multiply"
-	result = user_input_number_1 * user_input_number_2
-when "divide"
-	result = user_input_number_1.to_f / user_input_number_2.to_f
+def prompt(message)
+	puts("=> #{message}")
 end
 
-puts result
+def valid_number?(num)
+	num.to_i != 0
+end
+
+def operation_to_message(op)
+	case op
+	when '1'
+		"Adding"
+	when "2"
+		"Subtracting"
+	when "3"
+		"Multiplying"
+	when "4"
+		"Dividing"
+	end
+end
+
+prompt("Welcome to the Calculator! Enter your name: ")
+name = ''
+loop do
+	name = gets.chomp
+
+	if name.empty?
+		prompt("Make sure to use a valid name.")
+	else
+		break
+	end
+end
+
+loop do
+	prompt("Please input two numbers")
+
+	user_input_number_1 = ''
+	loop do
+		prompt("Number #1: ")
+		user_input_number_1 = gets.chomp.to_i
+
+		if valid_number?(user_input_number_1)
+			break
+		else
+			prompt("Hmm.... that doesn;t look like a valid number.")
+		end
+	end
+
+	user_input_number_2 = ""
+	loop do
+		prompt("Number #2: ")
+		user_input_number_2 = gets.chomp.to_i
+
+		if valid_number?(user_input_number_2)
+			break
+		else
+			prompt("Hmm... that doesn't look like a valid number.")
+		end
+	end
+
+	operator_prompt = <<-MSG
+		What operation would you like to perform?
+		1) Add
+		2) Subtract
+		3) Multiply
+		4) Divide
+	MSG
+	
+	prompt(operator_prompt)
+	user_input_operation = ""
+	
+	loop do
+		user_input_operation = gets.chomp
+		if %w(1 2 3 4).include?(user_input_operation)
+			break
+		else
+			prompt("Must choose 1, 2, 3, 4")
+		end
+	end
+
+	prompt("#{operation_to_message(user_input_operation)} the two numbers...")
+
+	result = case user_input_operation
+	when "1"
+		user_input_number_1 + user_input_number_2
+	when "2"
+		user_input_number_1 - user_input_number_2
+	when "3"
+		user_input_number_1 * user_input_number_2
+	when "4"
+		user_input_number_1.to_f / user_input_number_2.to_f
+	end
+
+	puts result
+	prompt("Do you want to perform another calculation? (Y to calculate again)")
+	answer = gets.chomp
+	break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for using the calculator!!!")
