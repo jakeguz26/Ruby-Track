6ae7bcef28d3aa65a79442e7c1b09db4b907db87
Jake Guzman <jacobguz26@gmail.com>
2022-11-14 12:53:39 -0700

    worked on more exercises.

---
+++ RB101/Lesson_6/join_method.rb
@@ -0,0 +1,15 @@
+def joinor(arr, delimiter=', ', word='or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
+end
+
+
+p joinor([1, 2, 3, 4, 5])                 
+
+
