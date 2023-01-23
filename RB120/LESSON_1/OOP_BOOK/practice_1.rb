94cde36bd84221301198e8eb3db884e2fd959b00
Jake Guzman <jacobguz26@gmail.com>
2023-01-23 05:04:53 -0700

    Worked on more exercises for RB120 Lessons 1 and 2.

--- RB120/LESSON_1/OOP_BOOK/practice_1.rb
+++ RB120/LESSON_1/OOP_BOOK/practice_1.rb
@@ -1,4 +1,17 @@
 class GoodDog
+  def self.what_am_i
+    puts "I'm a good dog class"
+  end
+
+  # def initialize
+  #   puts "im a good boy"
+  # end
+  
+  def woof
+    puts "woof"
+  end
+
 end
 
 thor = GoodDog.new
+thor.woof
