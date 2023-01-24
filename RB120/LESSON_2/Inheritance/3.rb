f4abc4f48072d4c32b648045654c4dd1ca814b39
Jake Guzman <jacobguz26@gmail.com>
2023-01-24 04:21:13 -0700

    Worked on some exercies for Lesson 2.

---
+++ RB120/LESSON_2/Inheritance/3.rb
@@ -0,0 +1,8 @@
+# Pet
+
+# Cat and Dog both inherit the behaviors in the Pet class
+#   - Cat and Dog both have access to #run and #jump
+
+# Cat has acces to #speak where the output is "meow"
+
+# Dog has access to #speak where the output is "bark" as well as #fetch and #swimming
