04cd6c918e759e774ed10c772ffd6db2be0ae4b3
Jake Guzman <jacobguz26@gmail.com>
2023-02-16 17:28:32 -0700

    Added more code to 21 projects.

--- RB120/LESSON_5/twenty_one.rb
+++ RB120/LESSON_5/twenty_one.rb
@@ -1,17 +1,29 @@
+FACE_CARDS = ['jack', 'queen', 'king', 'ace']
+
 class Participant
-  attr_accessor :cards_in_hand
+  attr_accessor :cards_in_hand, :move_choice
   
   def initialize
     @cards_in_hand = []
   end
 
   def hit
+    pull_from_deck
   end
 
   def stay
   end
 
-  def busted?
+  def busted?(arr)
+    sum_arr = []
+    arr.each do |element|
+      if FACE_CARDS.include?(element)
+        sum_arr << 10
+      else
+        sum_arr << element
+      end
+    end
+    sum_arr.sum > 21
   end
 
   def total
@@ -25,7 +37,7 @@ class Dealer < Participant
 end
 
 class Game
-  attr_reader :dealer, :player, :deck_of_cards
+  attr_accessor :dealer, :player, :deck_of_cards
 
   def initialize
     @dealer = Dealer.new
@@ -54,15 +66,51 @@ class Game
   end
 
   def show_initial_cards
-    puts "You have a #{player.cards_in_hand[0]} and a #{player.cards_in_hand[1]}."
-    puts "The dealer has a #{dealer.cards_in_hand[0]} and unknown."
+    puts "You have: #{player.cards_in_hand[0]} and #{player.cards_in_hand[1]}."
+    puts "The dealer has: #{dealer.cards_in_hand[0]} and unknown. #{dealer.cards_in_hand.size} total."
+    puts ""
+  end
+
+  def player_turn
+    loop do
+      move_choice = nil
+      loop do
+        puts "Would you like to HIT or STAY?"
+        move_choice = gets.chomp
+        if move_choice.downcase == 'hit' || move_choice.downcase == 'stay'
+          break
+        else
+          puts "INVALID INPUT"
+          puts ""
+        end
+      end
+      
+      player.move_choice = move_choice 
+
+      case player.move_choice
+      when 'hit'
+        hit
+        p player.busted?(player.cards_in_hand)
+      when 'stay'
+        break
+      end
+    end
+  end
+
+  def hit
+    player.cards_in_hand << pull_from_deck
+    display_player_cards
+    puts ""
+  end
+
+  def display_player_cards
+    puts "You now have: #{player.cards_in_hand[0..-2].join(', ')}" + " and #{player.cards_in_hand[-1]}."
   end
 
   def start
     initial_deal
     show_initial_cards
-    p deck_of_cards
-    # player_turn
+    player_turn
     # dealer_turn
     # show_result
   end
